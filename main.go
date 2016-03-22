cbf5c71e99fd9a9e152ea2b416298a775f356422
Stefan Naglee <stefannaglee@gmail.com>
2016-03-22 12:48:51 -0400

    Added initial registry communication tests and ability to pass registry urls from the command line

---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/stefannaglee/docker-registry-manager/registry"
+)
+
+// DuplicateFlags slice contains a list of registries to use.
+type DuplicateFlags []string
+
+// String is the method to format the flag's value, part of the flag.Value interface.
+func (d *DuplicateFlags) String() string {
+	return fmt.Sprint(*d)
+}
+
+// Set builds the registry slice by taking in the comma separated list and splitting on commas
+func (d *DuplicateFlags) Set(value string) error {
+	for _, url := range strings.Split(value, ",") {
+		*d = append(*d, url)
+	}
+	return nil
+}
+
+var logLevel int
+var registryFlags DuplicateFlags
+
+func init() {
+
+	// Set and parse the command line flags
+	flag.IntVar(&logLevel, "verbosity", 5, "Execution log level of the program: 1 = Panic Level, 2 = Fatal Level, 3 = Error Level, 4 = Warn Level, 5 = Info Level, 6 = Debug Level")
+	flag.Var(&registryFlags, "registry", "comma-separated list of registries to use. e.g https://host.domain:5000/v2/")
+	flag.Parse()
+
+	// Set the log level of the program
+	switch {
+	case logLevel == 1:
+		log.SetLevel(log.PanicLevel)
+	case logLevel == 2:
+		log.SetLevel(log.FatalLevel)
+	case logLevel == 3:
+		log.SetLevel(log.ErrorLevel)
+	case logLevel == 4:
+		log.SetLevel(log.WarnLevel)
+	case logLevel == 5:
+		log.SetLevel(log.InfoLevel)
+	case logLevel == 6:
+		log.SetLevel(log.DebugLevel)
+	}
+
+}
+
+func main() {
+
+	// Loop through the slice of passed registries and test their status
+	for _, regString := range registryFlags {
+		if err := registry.TestRegistryStatus(regString); err != nil {
+			// Notify of success
+			log.WithFields(log.Fields{
+				"Error": err,
+			}).Fatal("We are unable to make a connection to the registry!")
+
+			// Exit the program
+			os.Exit(1)
+		}
+	}
+
+}
---
+++ registry/registry.go
@@ -0,0 +1,103 @@
+package registry
+
+import (
+	"net"
+	"net/http"
+	"net/url"
+
+	log "github.com/Sirupsen/logrus"
+	_ "github.com/go-sql-driver/mysql" // need to initialize mysql before making a connection
+)
+
+// Registry contains all identifying information for communicating with a registry
+type Registry struct {
+	Name    string
+	IP      string
+	Scheme  string
+	Port    string
+	Version string
+}
+
+// GetURI returns the full url path for communicating with this registry
+func (r *Registry) GetURI() string {
+	return r.Scheme + "://" + r.Name + ":" + r.Port + "/v2"
+}
+
+// TestRegistryStatus takes in a registry URL and checks for communication errors
+//
+// Create and execute basic GET request to test if each registry can be reached
+// To determine registry status we test the base registry route of /v2/ and check
+// the HTTP response code for a 200 response (200 is a successful request)
+func TestRegistryStatus(registryURI string) error {
+
+	// Parse the registry string into our Registry type
+	registry := ParseRegistry(registryURI)
+	// Notify of initial attempt
+	log.WithFields(log.Fields{
+		"Registry URI": registryURI,
+	}).Info("Connecting to registry...")
+
+	// Create and execute a plain get request and check the http status code
+	response, err := http.Get(registry.GetURI())
+	if err != nil {
+		// Notify of error
+		log.WithFields(log.Fields{
+			"Registry URLs": registry,
+			"Error":         err,
+			"HTTP Response": response,
+			"Possible Fix":  "Check to see if your registry is up, and serving on the correct port with 'docker ps'.",
+		}).Fatal("Get request to registry timed out/failed! Is the URL correct, and is the registry active?")
+	} else if response.StatusCode != 200 {
+		// Notify of error
+		log.WithFields(log.Fields{
+			"Registry URLs": registry,
+			"HTTP Response": response.StatusCode,
+			"Possible Fix":  "Check to see if your registry is up, and serving on the correct port with 'docker ps'.",
+		}).Fatal("Get request to registry failed! Is the URL correct, and is the registry active?")
+	}
+
+	// Notify of success
+	log.WithFields(log.Fields{
+		"Registry Information": registry,
+		"Registry URI":         registry.GetURI(),
+	}).Info("Successfully connected to registry!")
+
+	return err
+}
+
+// ParseRegistry takes in a registry URI string and converts it into a registry object
+func ParseRegistry(registryURI string) Registry {
+
+	// Parse the URL and get the scheme
+	// e.g https, http, etc.
+	u, err := url.Parse(registryURI)
+	if err != nil {
+		log.Error(err)
+	}
+
+	// Get the host and port
+	// e.g test.domain.com and 5000, etc.
+	host, port, err := net.SplitHostPort(u.Host)
+	if err != nil {
+		log.Error(err)
+	}
+
+	// Lookup the ip for the passed host
+	// Using the host name try looking up the IP for informational purposes
+	ip, err := net.LookupHost(host)
+	if err != nil {
+		log.Error(err)
+	}
+
+	// Create a new registry type using all of the information queried above
+	r := Registry{
+		Name:    host,
+		Port:    port,
+		IP:      ip[0],
+		Scheme:  u.Scheme,
+		Version: "v2",
+	}
+
+	// Return the newly created registry type
+	return r
+}
