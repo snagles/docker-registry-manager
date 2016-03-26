4b70969a5d02a90d6f0aea2858d222e52d9d80cd
Stefan Naglee <stefannaglee@gmail.com>
2016-03-26 14:10:55 -0400

    Added support for getting tags by registry and repo name

---
+++ registry/tag.go
@@ -0,0 +1,61 @@
+package registry
+
+import (
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"net/http"
+
+	log "github.com/Sirupsen/logrus"
+)
+
+// Tags contains a slice of tags for the given repository
+// https://github.com/docker/distribution/blob/master/docs/spec/api.md#listing-image-tags
+type Tags struct {
+	Name string
+	Tags []string
+}
+
+// GetTags returns a slice of tags for a given repository and registry
+func GetTags(registryName string, repositoryName string) (Tags, error) {
+
+	// Check if the registry is listed as active
+	if _, ok := ActiveRegistries[registryName]; !ok {
+		return Tags{}, errors.New(registryName + " was not found within the active list of registries.")
+	}
+	r := ActiveRegistries[registryName]
+
+	// Create and execute Get request
+	response, err := http.Get(r.GetURI() + "/" + repositoryName + "/tags/list")
+	if err != nil {
+		log.WithFields(log.Fields{
+			"Registry URL": string(r.GetURI()),
+			"Error":        err,
+			"Possible Fix": "Check to see if your registry is up, and serving on the correct port with 'docker ps'. ",
+		}).Error("Get request to registry failed for the tags endpoint.")
+		return Tags{}, err
+	}
+
+	// Close connection
+	defer response.Body.Close()
+
+	// Read response into byte body
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"Error": err,
+			"Body":  body,
+		}).Error("Unable to read response into body!")
+		return Tags{}, err
+	}
+	ts := Tags{}
+	// Unmarshal JSON into the tag response struct containing an array of tags
+	if err := json.Unmarshal(body, &ts); err != nil {
+		log.WithFields(log.Fields{
+			"Error":         err,
+			"Response Body": string(body),
+		}).Error("Unable to unmarshal JSON!")
+		return ts, err
+	}
+	return ts, nil
+}
---
+++ registry/tag_test.go
@@ -0,0 +1,40 @@
+package registry
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+// TestGetTags passes the the GetTags function and registry and repository
+func TestGetTags(t *testing.T) {
+
+	// Create a new registry
+	rURI := "https://host.domain:5000"
+	r, err := ParseRegistry(rURI)
+
+	Convey("When we pass an valid RegistryURI we should get back the registry type with no errors", t, func() {
+		So(err, ShouldBeNil)
+		So(r, ShouldNotBeEmpty)
+	})
+
+	err = GetRegistryStatus(rURI)
+	// Test the response error
+	Convey("When we get the registry status there should be no errors and the registry should be added to the map of active registries", t, func() {
+		So(err, ShouldBeNil)
+		So(ActiveRegistries, ShouldContainKey, "host.domain")
+	})
+
+	repos, err := GetRepositories(r.Name)
+	Convey("When we get the repositories for this registry there should be no errors and there should be a slice of repositories", t, func() {
+		So(err, ShouldBeNil)
+		So(repos, ShouldNotBeEmpty)
+	})
+
+	tags, err := GetTags(r.Name, "tag")
+	Convey("When we get the tags for this repository there should be no errors and there should be a slice of tags", t, func() {
+		So(err, ShouldBeNil)
+		So(tags, ShouldNotBeEmpty)
+	})
+
+}
