6b4a31574364545751185f60f5a5b3057d314abe
Stefan Naglee <stefannaglee@gmail.com>
2016-04-10 15:14:43 -0400

    Added support for parsing local log files

--- app/utilities/log.go
+++ app/utilities/log.go
@@ -1,19 +1,59 @@
 package utils
 
 import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
 	"github.com/Sirupsen/logrus"
 	"github.com/rifflock/lfshook"
 )
 
+//{"level":"error","msg":"Test","time":"2016-04-10T12:05:30-04:00"}
+type LogEntry struct {
+	Level string
+	Msg   string
+	Time  time.Time
+}
+
 var Log = logrus.New()
 
 func init() {
-
 	Log.Formatter = &logrus.JSONFormatter{}
 	Log.Hooks.Add(lfshook.NewHook(lfshook.PathMap{
 		logrus.ErrorLevel: "./logs/error.log",
-		logrus.InfoLevel:  "./logs/info.log",
-		logrus.WarnLevel:  "./logs/warn.log",
-		logrus.FatalLevel: "./logs/fatal.log",
+		logrus.InfoLevel:  "./logs/error.log",
+		logrus.WarnLevel:  "./logs/error.log",
+		logrus.FatalLevel: "./logs/error.log",
+		logrus.PanicLevel: "./logs/error.log",
+		logrus.DebugLevel: "./logs/error.log",
 	}))
+}
+
+func ParseLogFile() []LogEntry {
+	file, err := os.Open("logs/error.log")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	es := []LogEntry{}
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		e := LogEntry{}
+		err := json.Unmarshal([]byte(scanner.Text()), &e)
+		if err != nil {
+			fmt.Println(err.Error)
+		}
+		es = append(es, e)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return es
 }
