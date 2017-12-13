1f4e09982e4c8edea5d4795330d753a682cd9efb
Stefan Naglee <stefannaglee@gmail.com>
2017-12-13 13:27:36 -0500

    Add cli package to use environment variables, update Dockerfile, compose file

--- app/main.go
+++ app/main.go
@@ -1,20 +1,21 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"net/url"
 	"os"
 	"path"
+	"path/filepath"
+	"runtime"
 	"strconv"
 	"time"
 
 	"github.com/astaxie/beego"
 	"github.com/sirupsen/logrus"
-	"github.com/snagles/docker-registry-manager/app/conf"
 	"github.com/snagles/docker-registry-manager/app/models"
 	_ "github.com/snagles/docker-registry-manager/app/routers"
 	"github.com/spf13/viper"
+	"github.com/urfave/cli"
 )
 
 const (
@@ -22,64 +23,74 @@ const (
 )
 
 func main() {
-	config := flag.String("config", "", "config path location")
-	version := flag.Bool("version", false, "app version")
-	flag.Parse()
+	app := cli.NewApp()
+	app.Name = "Docker Registry Manager"
+	app.Usage = "Connect to, view, and manage multiple private Docker registries"
 
-	if *version == true {
-		fmt.Println("Version: " + appVersion)
-		os.Exit(1)
-	}
-	c, err := parseConfig(*config)
-	if err != nil {
-		logrus.Fatal(err)
-	}
-
-	err = setlevel(c.App.LogLevel)
-	if err != nil {
-		logrus.Fatal(err)
+	var configPath string
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:        "config-path, c",
+			Usage:       "config path and name `/opt/docker-registry-manager/config.yml`",
+			EnvVar:      "REGISTRY_CONFIG",
+			Destination: &configPath,
+		},
 	}
 
-	for _, r := range c.Registries {
-		if r.URL != "" {
-			url, err := url.Parse(r.URL)
-			if err != nil {
-				logrus.Fatalf("Failed to parse registry from the passed url (%s): %s", r.URL, err)
-			}
-			port, err := strconv.Atoi(url.Port())
-			if err != nil || port == 0 {
-				logrus.Fatalf("Failed to add registry (%s), invalid port: %s", r.URL, err)
-			}
-			duration, err := time.ParseDuration(r.RefreshRate)
-			if err != nil {
-				logrus.Fatalf("Failed to add registry (%s), invalid duration: %s", r.URL, err)
-			}
-			if r.Password != "" && r.Username != "" {
-				if _, err := manager.AddRegistry(url.Scheme, url.Hostname(), r.Username, r.Password, port, duration, r.SkipTLS); err != nil {
-					logrus.Fatalf("Failed to add registry (%s): %s", r.URL, err)
+	app.Action = func(ctx *cli.Context) {
+		c, err := parseConfig(configPath)
+		if err != nil {
+			logrus.Fatal(err)
+		}
+
+		err = setlevel(c.App.LogLevel)
+		if err != nil {
+			logrus.Fatal(err)
+		}
+
+		for _, r := range c.Registries {
+			if r.URL != "" {
+				url, err := url.Parse(r.URL)
+				if err != nil {
+					logrus.Fatalf("Failed to parse registry from the passed url (%s): %s", r.URL, err)
 				}
-			} else {
-				if _, err := manager.AddRegistry(url.Scheme, url.Hostname(), "", "", port, duration, r.SkipTLS); err != nil {
-					logrus.Fatalf("Failed to add registry (%s): %s", r.URL, err)
+				port, err := strconv.Atoi(url.Port())
+				if err != nil || port == 0 {
+					logrus.Fatalf("Failed to add registry (%s), invalid port: %s", r.URL, err)
+				}
+				duration, err := time.ParseDuration(r.RefreshRate)
+				if err != nil {
+					logrus.Fatalf("Failed to add registry (%s), invalid duration: %s", r.URL, err)
+				}
+				if r.Password != "" && r.Username != "" {
+					if _, err := manager.AddRegistry(url.Scheme, url.Hostname(), r.Username, r.Password, port, duration, r.SkipTLS); err != nil {
+						logrus.Fatalf("Failed to add registry (%s): %s", r.URL, err)
+					}
+				} else {
+					if _, err := manager.AddRegistry(url.Scheme, url.Hostname(), "", "", port, duration, r.SkipTLS); err != nil {
+						logrus.Fatalf("Failed to add registry (%s): %s", r.URL, err)
+					}
 				}
 			}
 		}
+
+		// Beego configuration
+		beego.BConfig.AppName = "docker-registry-manager"
+		beego.BConfig.RunMode = "dev"
+		beego.BConfig.Listen.EnableAdmin = true
+		beego.BConfig.CopyRequestBody = true
+		beego.BConfig.WebConfig.ViewsPath = "views"
+
+		// add template functions
+		beego.AddFuncMap("shortenDigest", DigestShortener)
+		beego.AddFuncMap("statToSeconds", StatToSeconds)
+		beego.AddFuncMap("bytefmt", ByteFmt)
+		beego.AddFuncMap("bytefmtdiff", ByteDiffFmt)
+		beego.AddFuncMap("timeAgo", TimeAgo)
+		beego.AddFuncMap("oneIndex", func(i int) int { return i + 1 })
+		beego.Run()
 	}
-
-	// Beego configuration
-	beego.BConfig.AppName = "docker-registry-manager"
-	beego.BConfig.RunMode = "dev"
-	beego.BConfig.Listen.EnableAdmin = true
-	beego.BConfig.CopyRequestBody = true
-
-	// add template functions
-	beego.AddFuncMap("shortenDigest", DigestShortener)
-	beego.AddFuncMap("statToSeconds", StatToSeconds)
-	beego.AddFuncMap("bytefmt", ByteFmt)
-	beego.AddFuncMap("bytefmtdiff", ByteDiffFmt)
-	beego.AddFuncMap("timeAgo", TimeAgo)
-	beego.AddFuncMap("oneIndex", func(i int) int { return i + 1 })
-	beego.Run()
+	app.Run(os.Args)
 }
 
 func setlevel(level string) error {
@@ -125,12 +136,18 @@ func parseConfig(configPath string) (*config, error) {
 		base := path.Base(configPath)
 		ext := path.Ext(configPath)
 		v.SetConfigName(base[0 : len(base)-len(ext)])
+		logrus.Infof("Using config located in %s with config name %s", path.Dir(configPath), base[0:len(base)-len(ext)])
 	} else {
-		// use the default tree
 		v.SetConfigName("config")
-		v.AddConfigPath(conf.GOPATH + "/src/github.com/snagles/docker-registry-manager")
-		v.AddConfigPath("../")
-		v.AddConfigPath("/opt/docker-registry-manager")
+		var root string
+		_, r, _, ok := runtime.Caller(0)
+		if ok {
+			root = filepath.Dir(r)
+			v.AddConfigPath(root)
+		} else {
+			logrus.Fatalf("Failed to get runtime caller for parser")
+		}
+		logrus.Infof("Using config located in %s with config name %s", root, "config.yml")
 	}
 
 	if err := v.ReadInConfig(); err != nil {
